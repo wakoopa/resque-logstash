a6cdbc1c28228e7cf8e97769a5097031dd2f6b43
Eugene Pimenov <eugene@libc.st>
2013-12-11 17:13:11 +0100

    Run Resque::Plugin.lint in specs
    Resque plugin guide recommends it.

--- spec/logstash_spec.rb
+++ spec/logstash_spec.rb
@@ -11,6 +11,10 @@ describe Resque::Logstash do
     Resque::Logstash.transport = double(:push => nil)
   end
 
+  it 'complient with resque plugin policy' do
+    expect { Resque::Plugin.lint(Resque::Logstash) }.not_to raise_error
+  end
+
   describe '#around_perform_logstash_measure' do
     it 'calls logstash_push_time with the duration' do
       expect(job).to receive(:logstash_push_duration).with(be_within(0.01).of(0.3), [])
